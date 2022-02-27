b14f9391f5de24123da44b41cd31358fe0208004
KwakYV <kvakyur@gmail.com>
2022-02-27 12:34:38 +0300

    Init commit

---
+++ src/main/java/com/example/gbmongo/GbMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gbmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gbmongo/GbMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gbmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
